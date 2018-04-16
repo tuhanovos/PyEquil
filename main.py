1513cf7c7e8fe168eac1d57898d391d116a99bea
tuhanovos <saitpage2011@yandex.ru>
2018-04-16 20:53:37 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Created on 9 апр. 2018 г.
+
+@author: eq
+'''
+from server.server import MyClass
+
+if __name__ == '__main__':
+    m = MyClass('Hello')
+    m.construct()
+            
+            
---
+++ server/server.py
@@ -0,0 +1,18 @@
+'''
+Created on 15 апр. 2018 г.
+
+@author: eq
+'''
+from twisted.python.test.deprecatedattributes import message
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    def __init__(self, message):
+        MyClass.message = message
+    
+    @classmethod
+    def construct(cls):
+        heading = input('Input heading: ')
+        print(cls.message, heading)
