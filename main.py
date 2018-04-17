e2d6860179eb67442b8301f55d53e22223a946c4
tuhanovos <saitpage2011@yandex.ru>
2018-04-17 04:05:15 +0300

    add file firstDublicate.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ Created on 9 апр. 2018 г.
 
 @author: eq
 '''
-from server.server import MyClass
 
+from firstDublicate import firstDuplicate   
+    
+    
 if __name__ == '__main__':
-    m = MyClass()
-    m.construct()
-    m1 = MyClass()
-    m1.construct()    
+    a = [8, 4, 6, 2, 6, 4, 7, 9, 5, 8]
+    print(firstDuplicate(a))
             
